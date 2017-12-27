db7d34595c6780aa9bb93e7e714ffd18f2e105a7
haduchuy96 <haduchuy96@gmail.com>
2017-12-27 16:01:30 +0700

    Edit URL mongoDB to online

--- config.js
+++ config.js
@@ -1,5 +1,5 @@
 module.exports = {
-    default_connect_string: 'mongodb://localhost:27017/blockchain',
+    default_connect_string: 'mongodb://blockchain:123456789@ds133127.mlab.com:33127/blockchain',
     jwt_secret_key: '16544saf34fas1v5as4',
     allow_origin_host: 'http://localhost:3000'
 }

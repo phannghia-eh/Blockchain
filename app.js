a01d0ce55798274953b9ed06451f1ee01de17da6
vanzez <phn1910@gmail.com>
2018-01-10 10:35:46 +0700

    allow live host change live time of JWT

--- app.js
+++ app.js
@@ -20,7 +20,7 @@ var app = express();
 app.use(mwWebSocket.Listen);
 
 app.use(function(req, res, next){
-  res.setHeader('Access-Control-Allow-Origin', config.allow_origin_host);
+  res.setHeader('Access-Control-Allow-Origin', '*');
   res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PATCH, PUT, DELETE, OPTIONS');
   res.setHeader("Access-Control-Allow-Headers", "Content-Type, Authorization,x-access-token");
   if(req.method === 'OPTIONS') return res.end();
